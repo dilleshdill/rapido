efd0e26034c65bbd6ac435149772026891c4d17b
haritarun <tarunbommana798@gmail.com>
2025-09-06 10:59:47 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 import express from "express";
 import cors from "cors";
+import { Server } from "socket.io";
+import http from 'http'
 import userRoutes from "./routes/userRoutes.js";
 import uploadRoutes from "./routes/uploadRoute.js";
 import driverRoutes from "./routes/driverRoute.js";
 import checkCordinates from "./routes/checkCordinatesRoute.js";
 import ridesRoute from "./routes/ridesRouter.js";
+import driverSocket from "./sockets/driverSocket.js";
 
 const app = express();
+const server = http.createServer(app); 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",   
+    methods: ["GET", "POST"]
+  }
+});
+
+export default io
+
+driverSocket(io)
 
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
