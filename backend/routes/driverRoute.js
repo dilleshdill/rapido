8d8607778fce4b837f46ede43f38b87834495bf0
haritarun <tarunbommana798@gmail.com>
2025-09-09 22:55:55 +0530

    ...

--- backend/routes/driverRoute.js
+++ backend/routes/driverRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {addDriver,driverLogin,driverRides} from "../controllers/driverController.js";
+import authenticate from "../middleware/authentication.js";
 const driverRoutes = express.Router();
 
 // Example route for driver
 driverRoutes.post("/add-driver", addDriver);
 driverRoutes.post("/login",driverLogin)
-driverRoutes.get("/all-rides",driverRides)
+driverRoutes.get("/all-rides",authenticate,driverRides)
 export default driverRoutes;
